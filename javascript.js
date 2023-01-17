1b2a583b1a5ea0bf0b4b85103a6c7de71c06f79b
Edwin E <edwin.emilien92@gmail.com>
2023-01-16 20:33:44 -0500

    Create grid in contain

--- javascript.js
+++ javascript.js
@@ -1,16 +1,27 @@
-let columns = 5;
-let rows = 5;
+let box = 16;
+let columns = box;
+let rows = box;
 
 let grid = document.createElement('div');
 grid.className = 'grid';
+
+let containerHeight = 500;
+let containerWidth = 500;
+
+grid.style.maxHeight= containerHeight + "px";
+grid.style.maxWidth = containerWidth + "px";
+
+
 for (let i = 0; i < columns; ++i) {
     let column = document.createElement('div'); // create column
     column.className = 'column';
     for (let j = 0; j < rows; ++j) {
         let row = document.createElement('div'); // create row
         row.className = 'row';
-        let divRow = document.getElementsByClassName("row"); 
-        //row.textContent = i + '-' +j; // set text
+        row.style.height = (containerHeight/rows) + "px";
+        row.style.width = (containerWidth/rows) + "px";
+        row.style.borderStyle = "solid";
+        row.style.borderWidth = "1px";
         column.appendChild(row); // append row in column
     }
     grid.appendChild(column); // append column inside grid
